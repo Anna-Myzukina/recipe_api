10584d3b60ee88a96eb282fa087a1cf5e7a0c146
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-07-18 13:19:21 +0300

    Add tests to routing

--- spec/routing/recipes_routing_spec.rb
+++ spec/routing/recipes_routing_spec.rb
@@ -3,33 +3,27 @@ require 'rails_helper'
 RSpec.describe Api::V1::RecipesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: 'api/v1/recipes').to
-      route_to('api/v1/recipes#index')
+      expect(get: 'api/v1/recipes').to route_to('api/v1/recipes#index')
     end
 
     it 'routes to #show' do
-      expect(get: 'api/v1/recipes/1').to
-      route_to('api/v1/recipes#show', id: '1')
+      expect(get: 'api/v1/recipes/1').to route_to('api/v1/recipes#show', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: 'api/v1/recipes').to
-      route_to('api/v1/recipes#create')
+      expect(post: 'api/v1/recipes').to route_to('api/v1/recipes#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: 'api/v1/recipes/1').to
-      route_to('api/v1/recipes#update', id: '1')
+      expect(put: 'api/v1/recipes/1').to route_to('api/v1/recipes#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: 'api/v1/recipes/1').to
-      route_to('api/v1/recipes#update', id: '1')
+      expect(patch: 'api/v1/recipes/1').to route_to('api/v1/recipes#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: 'api/v1/recipes/1').to
-      route_to('api/v1/recipes#destroy', id: '1')
+      expect(delete: 'api/v1/recipes/1').to route_to('api/v1/recipes#destroy', id: '1')
     end
   end
 end
